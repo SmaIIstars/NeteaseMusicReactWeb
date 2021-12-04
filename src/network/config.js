132ac978a1b0ea2b44af58a7a4d890ed2261d550
hetanran <hetanran@bytedance.com>
2021-12-04 21:48:05 +0800

    fix error words

--- src/network/config.js
+++ src/network/config.js
@@ -1,5 +1,5 @@
-const devBaseURL = "http://120.79.65.137:4000/";
-const proBaseURL = "http://120.79.65.137:4000/";
+const devBaseURL = "http://123.45.67.89:8000/";
+const proBaseURL = "http://123.45.67.89:8000/";
 
 export const BASE_URL =
   process.env.NODE_ENV === "development" ? devBaseURL : proBaseURL;
